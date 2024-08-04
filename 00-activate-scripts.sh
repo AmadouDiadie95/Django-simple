8b6723cb9d12c2497703d593bf5c783238df097e
Amadou Diadie Coulibaly <96771479+AmadouDiadie95@users.noreply.github.com>
2024-08-04 13:32:58 +0000

    create scripts 10

--- 00-activate-scripts.sh
+++ 00-activate-scripts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-chmod 777 01-git-pull.sh 03-create-venv.sh 04-install-requirements.sh 05-start-django.sh 06-install-net-tools.sh 07-netstat-view.sh 08-socat-ipv6.sh
+chmod 777 01-git-pull.sh 02-git-clone.sh 03-create-venv.sh 04-install-requirements.sh 05-start-django.sh 06-install-net-tools.sh 07-netstat-view.sh 08-socat-ipv6.sh
 
--- 02-git-clone.sh
+++ 02-git-clone.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git clone https://oauth2:ghp_sRAKpfZCtprkY7Wb9faWX3dyMRyv8J4Zx@github.com/Daamtu/DaamtuGen-AI-Backend.git
+git clone https://oauth2:ghp_sRAKpfZCtprkY7Wb9faWX3dyMRyv8J4ZxTe@github.com/Daamtu/DaamtuGen-AI-Backend.git
 
